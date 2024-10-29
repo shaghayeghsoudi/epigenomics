7ffb6a9800f8ccbe13e63be4204bb3cfee00dbd9
shaghayeghsoudi <annasoudi@gmail.com>
2024-10-29 14:53:45 -0700

    updated with removing repeated fastq files

--- DNAmethylation/slurm/merge_fastq_files_in_different_lanes.sh
+++ DNAmethylation/slurm/merge_fastq_files_in_different_lanes.sh
@@ -1,3 +1,21 @@
+#!/bin/bash -l
+# ---------------------------------------------------------------------
+# SLURM script for demultiplexed fumi_tools
+# ---------------------------------------------------------------------
+#SBATCH --job-name==fumi_tools
+#SBATCH --cpus-per-task=2
+#SBATCH --mem-per-cpu=14G
+#SBATCH --tasks=1
+#SBATCH --nodes=1
+#SBATCH --time=24:00:00
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=shsoudi
+#SBATCH --partition=emoding
+#SBATCH --export=ALL
+
+
+
+
 # Define the folder containing your fastq.gz files
 FOLDER="/oak/stanford/groups/emoding/analysis/shaghayegh/methylation/NovaSeqX15-22_processing_upstream/test"
 
@@ -15,7 +33,11 @@ for prefix in $(ls *_R1.fastq.gz | sed -E 's/_L00[0-9]+_R1.fastq.gz//' | sort | 
     # Concatenate R1 files
     cat "${R1_files[@]}" > "$R1_merged"
     echo "Merged R1 files for $prefix into $R1_merged"
+
+    rm "${R1_files[@]}"
+    echo "Removed original R1 files for $prefix"
 done
+
 
 for prefix in $(ls *_R2.fastq.gz | sed -E 's/_L00[0-9]+_R2.fastq.gz//' | sort | uniq -d); do
     # Collect all R1 files with the repeated prefix
@@ -27,5 +49,11 @@ for prefix in $(ls *_R2.fastq.gz | sed -E 's/_L00[0-9]+_R2.fastq.gz//' | sort | 
     # Concatenate R1 files
     cat "${R2_files[@]}" > "$R2_merged"
     echo "Merged R2 files for $prefix into $R2_merged"
+
+
+    rm "${R2_files[@]}"
+    echo "Removed original R2 files for $prefix"
+
+
 done
 
